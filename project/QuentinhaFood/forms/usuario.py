3bdef579b9cf7101fc48ac3891e6ef1454876ac0
Gabriel Duarte <gabriel.duarte@ultralims.com.br>
2018-11-19 18:52:18 -0200

    Base html layout

--- project/QuentinhaFood/forms/usuario.py
+++ project/QuentinhaFood/forms/usuario.py
@@ -1,14 +1,32 @@
 from django import forms
 from ..models import Usuario
 
+
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    confirmar_password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'validate', 'id': 'username', 'v-model': 'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'validate', 'id': 'password', 'v-model': 'password'}))
+    confirmar_password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'validate', 'id': 'confirmar_password', 'v-model': 'confirmar_password'}))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={'class': 'validate', 'id': 'email', 'v-model': 'email'}))
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'validate', 'id': 'first_name', 'v-model': 'first_name'}))
+    cpf = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'validate', 'id': 'cpf', 'v-model': 'cpf'}))
+    telefone_usuario = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'validate', 'id': 'telefone_usuario', 'v-model': 'telefone_usuario'}))
+    telefoneAlternativo_usuario = forms.CharField(
+        widget=forms.TextInput(attrs={'id': 'telefoneAlternativo_usuario', 'v-model': 'telefoneAlternativo_usuario'}))
+    # imagem_usuario = forms.CharField(
+    #     attrs={'class': 'validate', 'id': 'imagem_usuario'})
+
     class Meta:
         model = Usuario
-        fields = ('username', 'password', 'confirmar_password', 'email', 'first_name', 'cpf', 'telefone_usuario', 
-        'telefoneAlternativo_usuario', 'imagem_usuario')
-    
+        fields = ('username', 'password', 'confirmar_password', 'email', 'first_name', 'cpf', 'telefone_usuario',
+                  'telefoneAlternativo_usuario', 'imagem_usuario')
+
     def clean(self):
         cleaned_data = super(UserForm, self).clean()
         password = cleaned_data.get("password")
@@ -17,4 +35,3 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(
                 "password and confirm_password does not match"
             )
-    
