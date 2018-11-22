dd9fb5fb44da9c77ef53a6385f7261b4d73dbec2
Leonardo de Miranda Möers <moers.lm@gmail.com>
2018-11-22 08:38:10 -0200

    criado a edição de cadastro de usuario

--- project/QuentinhaFood/views/usuario.py
+++ project/QuentinhaFood/views/usuario.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from ..forms.usuario import UserForm, UpdateUser
 from ..models.usuario import Usuario
 from django.contrib.auth import authenticate, login
+from django.views.generic.base import View
 
-def cadastroUser(request):
-    template = 'usuario/cadastro.html'
-    if request.method == 'POST':
-        form = UserForm(request.POST, request.FILES)
-        if form.is_valid():
-            user = form.save()
-            user.set_password(user.password)
-            user.save()
-            login(request, user)
-            return HttpResponseRedirect('/lista_estabelecimento/')
+
+class CadastroUsuario(View):
+    template_name = 'usuario/cadastro.html'
+
+    def get(self, request, user_id=None):
+        if user_id:
+            userObj = Usuario.objects.get(id=user_id)
+            form = UpdateUser(instance=userObj)
+            editar = True
         else:
-            print(form.errors)
-    else:
-        form = UserForm()
+            form = UserForm()
+            editar = False
+        return render(request, self.template_name, {'form': form, 'editar': editar, 'user_id': user_id})
 
-    context_dict = {'form': form}
-    return render(request, template, context_dict)
+    def post(self, request):
+        if request.method == 'POST':
+            user_id = request.POST['user_id']
 
-def updateUser(request, id):
-    template = 'usuario/update_usuario.html'
-    if request.method == 'POST':
-        #form = UserForm(data=request.POST, files=request.FILES, instance = Usuario.objects.get(id=id))
-        Usuario.objects.filter(id=id).update(**request.POST)        
-    else:
-        form = UserForm(instance = Usuario.objects.get(id=id))
-        
-    context_dict = {'form': form}
-    return render(request, template, context_dict)
-    
+            if user_id:
+                userObj = Usuario.objects.get(id=user_id)
+                form = UpdateUser(instance=userObj, data=request.POST)
+            else:
+                form = UserForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                user = form.save()
+                msg = "Seus dados foram atualizados!"
+
+                if 'imagem_usuario' in request.FILES:
+                    user.imagem_usuario = request.FILES['imagem_usuario']
+
+                user.save()
+
+                return render(request, self.template_name, {'form': form, 'msg': msg})
+            else:
+                form = UserForm()
+                msg = "Ocorreu um erro!"
+                return render(request, self.template_name, {'form': form, 'msg': msg})
+        else:
+            form = UserForm()
+            return render(request, self.template_name, {'form': form, 'msg': msg})
