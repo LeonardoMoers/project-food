6209c0c99344c44a299ea5eb79cd97db936f922f
Gabriel Duarte <gabriel.duarte@ultralims.com.br>
2018-11-26 00:02:09 -0200

    nova tela de estabelecimento

--- project/QuentinhaFood/forms/cadastro_produto.py
+++ project/QuentinhaFood/forms/cadastro_produto.py
@@ -2,6 +2,7 @@ from django import forms
 from ..models import Produto, SubCategoria, Categoria
 
 class ProdutoForm(forms.ModelForm):
+    
     class Meta:
         model = Produto
         fields = ('categoria_idCategoria', 'subCategoria_idsubCategoria', 'nome_produto', 'descricao_produto', 'adicionais_produto', 'imagem_produto')
